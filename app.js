033bca01ceb345d8114555c81a83364592f87319
umairaslam2 <154333213+umairaslam2@users.noreply.github.com>
2024-02-26 22:23:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,105 @@
+//chapter1
+
+//question # 01;
+
+//alert ("Error! Please enter a valid password")
+
+//question # 02;
+
+//alert ("Welcome to JS Land...\n Happy Coding! ")
+
+//question # 03;
+
+//alert ("Welcome to JS Land... ")
+
+//alert (" Happy Coding! ")
+
+//question # 04;
+
+//alert (" Hello....I can run JS through my web browser console ")
+
+
+//chapter2;
+
+//question # 01;
+
+//var username
+
+//question # 02;
+
+//var myName="Umair Aslam";
+
+//question # 03;
+
+//var message ="Hello World"
+
+//alert (message);
+
+//question # 04;
+
+//var name="Jhone Doe"
+
+//alert (name);
+
+//var age="15 years old"
+
+//alert (age)
+
+//var carrer="Certified Mobile App Development"
+
+//alert (carrer)
+
+//question # 05;
+
+//var pizza= "Pizza\n Pizz\n Piz "
+
+//alert (pizza)
+
+//question # 06;
+
+//var Email="example@example.com"
+
+//alert ("My Email Address is " + Email)
+
+//question # 07;
+
+//var book ="A smarter way to learn Java Script"
+
+//alert ("I am try to learn from book"+book)
+
+//question # 08;
+
+
+//document.write("yeh I can write Html content through JavaScript")
+
+//question # 09;
+
+//alert ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+
+//chapter3;
+
+//question # 01;
+
+
+//var age="I am 15 years old"
+
+//alert (age)
+
+
+//question # 02;
+
+//var no="14";
+
+//alert ("You have visited this site "+ no+" times")
+
+//question # 03;
+
+//var birthYear ="2002";
+
+
+//document.write("My birth year is " + birthYear+" \ndata type of mny decleared variable is number" )
+
+//question # 04;
+
+//document.write("John Doe ordered 5 T-shirt(s) on XYZ Clothing store" )
